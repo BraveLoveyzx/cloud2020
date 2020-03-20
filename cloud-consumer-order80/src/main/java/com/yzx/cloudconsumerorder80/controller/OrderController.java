342a7e59e47c668814c727677e15c978e0736e15
mastermind <1936513546@qq.com>
2020-03-20 08:14:54 +0800

    mastermind

---
+++ cloud-consumer-order80/src/main/java/com/yzx/cloudconsumerorder80/controller/OrderController.java
@@ -0,0 +1,45 @@
+package com.yzx.cloudconsumerorder80.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+
+    /**
+     * @Description: 新增
+     * @Param:
+     * @return:
+     * @Author: mastermind
+     * @Date: 2020-03-20 07:47
+     */
+    @PostMapping("/add")
+    public String add() {
+
+        //调用8001新增
+
+        return "success";
+    }
+
+
+    /**
+     * @Description: 查询
+     * @Param:
+     * @return:
+     * @Author: mastermind
+     * @Date: 2020-03-20 07:50
+     */
+    @GetMapping("/query")
+    public String query(Long id) {
+
+        //调用8001查询
+
+        return "success";
+    }
+
+
+}
